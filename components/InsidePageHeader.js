5129bb0b43c96cff5d68d71311910130f645e477
Raul Smith <raul.n.smith@gmail.com>
2022-12-05 11:39:35 -0700

    Changes to About Page

---
+++ components/InsidePageHeader.js
@@ -0,0 +1,9 @@
+const InsidePageHeader = ({ title }) => {
+    return (
+        <div className="col mb-5 p-5 bg-jumbo text-white">
+            <h1 className="display-1 text-shadow-1">{title}</h1>
+        </div>
+    )
+}
+
+export default InsidePageHeader;
