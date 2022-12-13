6480ce4e8f2e46865bc53a281c80b9d3c30849bb
Raul Smith <raul.n.smith@gmail.com>
2022-12-12 22:52:17 -0700

    Add Meta description

--- pages/_app.js
+++ pages/_app.js
@@ -4,6 +4,7 @@ import Footer from '../components/Footer';
 import '../styles/owl.css';
 import { useEffect } from 'react';
 import Transition from '../components/Transition';
+import Head from 'next/head';
 
 function MyApp({ Component, pageProps }) {
 
@@ -12,24 +13,32 @@ function MyApp({ Component, pageProps }) {
   }, [])
 
   return (
-    <div className="container-fluid px-0">
-      <main className="main">
-        <Header />
-        <div className="row mx-0">
-          <div className="col px-0">
-            <Transition>
-              <Component {...pageProps} />
-            </Transition>
+    <>
+      <Head>
+        <title>Galactic Digital Studios</title>
+        <meta name="description" content="Galactic Digital Studios is your one-stop shop for website development, app development, search engine optimization, content management, database management, graphic and logo design, and much more!" />
+        <meta name="keywords" content="website design, website development, web development, web design, mobile app design, mobile app development, graphic design, logo design, content management, database management, search engine optimization, SEO, CMS, Salt Lake City, Ogden, Provo, Denver, Seattle, Portland, Tacoma, Beaverton, Boise" />
+        <link rel="icon" href="favicon.ico" />
+      </Head>
+      <div className="container-fluid px-0">
+        <main className="main">
+          <Header />
+          <div className="row mx-0">
+            <div className="col px-0">
+              <Transition>
+                <Component {...pageProps} />
+              </Transition>
+            </div>
           </div>
-        </div>
-        <div className="row mb-3 pb-3 mx-0">
-          <div className="col px-0">
-            &nbsp;
+          <div className="row mb-3 pb-3 mx-0">
+            <div className="col px-0">
+              &nbsp;
+            </div>
           </div>
-        </div>
-        <Footer />
-      </main>
-    </div>
+          <Footer />
+        </main>
+      </div>
+    </>
   )
 }
 
