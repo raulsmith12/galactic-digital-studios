112a0846b910c7930dd27132b890c0d8111d2fcc
Raul Smith <raul.n.smith@gmail.com>
2023-03-14 15:11:49 -0600

    Sitemap fix

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,5 +1,19 @@
+const siteUrl = 'https://galacticdigitalstudios.com/';
 module.exports = {
-    siteUrl: 'https://galacticdigitalstudios.com',
+    siteUrl,
+    exclude: ["/404"],
     generateRobotsTxt: true,
-    sitemapSize: 7000,
-}
+    robotsTxtOptions: {
+      policies: [
+        {
+          userAgent: "*",
+          disallow: ["/404"],
+        },
+        { userAgent: "*", allow: "/" },
+      ],
+      additionalSitemaps: [
+        `${siteUrl}sitemap.xml`,
+        `${siteUrl}server-sitemap.xml`,
+      ],
+    },
+  }
