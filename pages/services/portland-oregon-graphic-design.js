419e024af6a3971d24cf6177ab5576a25173b3a4
Raul Smith <raul.n.smith@gmail.com>
2023-04-26 19:54:48 -0600

    SEO Phase 3.3

---
+++ pages/services/portland-oregon-graphic-design.js
@@ -0,0 +1,97 @@
+import Head from "next/head"
+import Script from "next/script"
+import InsidePageHeader from "../../components/InsidePageHeader"
+import RequestForm from "../../components/RequestForm"
+
+const Page = () => {
+    return (
+        <>
+            <Head>
+                <title>Portland Oregon Graphic Design - Galactic Digital Studios</title>
+                <meta name="description" content="Business cards. Brochures. Banners. Social media graphics. Flyers. Bookmarks. Galactic Digital Studios can do all of these and more. Ask a professional member of our team about how we can help your business grow." />
+                <meta name="keywords" content="graphic design, flyer design, banner design, bookmark design, Portland, Oregon" />
+
+                <meta property="og:type" content="website" />
+                <meta property="og:url" content="https://galacticdigitalstudios.com/services/portland-oregon-graphic-design" />
+                <meta property="og:title" content="Portland Oregon Graphic Design - Galactic Digital Studios" />
+                <meta property="og:description" content="Business cards. Brochures. Banners. Social media graphics. Flyers. Bookmarks. Galactic Digital Studios can do all of these and more. Ask a professional member of our team about how we can help your business grow." />
+                <meta property="og:image" content="https://galacticdigitalstudios.com/img/gds-logo-short-transparent.png" />
+
+                <meta property="twitter:card" content="summary_large_image" />
+                <meta property="twitter:url" content="https://galacticdigitalstudios.com/services/portland-oregon-graphic-design" />
+                <meta property="twitter:title" content="Portland Oregon Graphic Design - Galactic Digital Studios" />
+                <meta property="twitter:description" content="Business cards. Brochures. Banners. Social media graphics. Flyers. Bookmarks. Galactic Digital Studios can do all of these and more. Ask a professional member of our team about how we can help your business grow." />
+                <meta property="twitter:image" content="https://galacticdigitalstudios.com/img/gds-logo-short-transparent.png" />
+                <link rel="icon" href="favicon.ico" />
+            </Head>
+            <Script strategy="afterInteractive" src="https://www.googletagmanager.com/gtag/js?id=G-B8904LJMQQ"/>
+            <Script
+                id='google-analytics'
+                strategy="afterInteractive"
+                dangerouslySetInnerHTML={{
+                    __html: `
+                    window.dataLayer = window.dataLayer || [];
+                    function gtag(){dataLayer.push(arguments);}
+                    gtag('js', new Date());
+                    gtag('config', 'G-B8904LJMQQ', {
+                        page_path: window.location.pathname,
+                    });
+                    `,
+                }}
+            />
+            <div className="container-fluid pb-5 mb-5">
+                <div className="row">
+                    <InsidePageHeader title="Portland Oregon Graphic Design" />
+                </div>
+                <div className="row">
+                    <div className="col">
+                        <h2>Graphic Design Including Banners, Bookmarks, and More for Portland</h2>
+                    </div>
+                </div>
+                <div className="row">
+                    <div className="col-md-3 d-none d-md-block">
+                        <div className="container-fluid">
+                            <div className="row">
+                                <div className="col text-center">
+                                    <img src="https://galacticdigitalstudios.com/img/graphic-design-l.jpg" width="100%" alt="Laptop with trendy graphic design poster behind it" />
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="col-md-9 col-sm-12">
+                        <p className="h5">There are many applications to great graphic design. Graphic design is really all around us, when you think about it. Billboards, banner ads, websites, social media, books, even applications on your mobile device, all have graphic design elements about them. And when it comes to finding a great graphic design company that will deal with small businesses, it can be a tremendous challenge.</p>
+                        <p className="h5">Now, with small business graphic design from Galactic Digital Studios available in the Portland, Oregon area, this service is much more readily available to you. Portland, Oregon is one of the most naturally beautiful regions in the country. Only miles away from both the sandy beaches of Seaside and the breathtaking sights of Multnomah Falls, Portland, Oregon is often referred to as the &ldquo;weird&rdquo; city for its interesting and unusual landscape.</p>
+                        <p className="h5">Even taking a trip to Portland, Oregon really drives home the importance of great graphic design. Everywhere you go in town, there are reminders of the need for this. When you go to the Saturday Market, you see people with signs and banners looking to advertise for their business. When you go to the zoo or the International Rose Test Garden, there are reminders of what exhibit you&rsquo;re in or where you need to go.</p>
+                        <p className="h5">But perhaps one of the greatest reminders of the need for great graphic design can be found in Old Town in Portland, Oregon: Voodoo Doughnuts, a Portland, Oregon staple. Even as you&rsquo;re struggling to find validated parking in the area, you can tell right away what you&rsquo;re looking for and where to find it. The pink box that the donuts come in, the signage outside the small building, and the menu once you enter the building (and even the one on the side of the building that helps patrons decide what they want) are all great examples of graphic design being used to help advertise a business.</p>
+                        <p className="h5">When considering working with a small business graphic design company, take into great consideration that you want a business that understands the everyday struggles that a small business goes through. You want a graphic design business that is flexible, seeks out understanding of the local industry and what the latest trends are, will fully support local business first, and is willing to work with you on building your printed and digital graphic design needs.</p>
+                        <p className="h5">Our graphic design team is fully capable and equipped to handle any and all of your digital and printed graphic design needs. If you are looking for printed cups or packages for your coffee or confectionery shop, we have design professionals that can handle this. If you are looking for a banner or sign to let people know where you are, this is also something that we can handle. If you are merely looking for business cards, Galactic Digital Studios is even capable of helping design those, as well.</p>
+                        <p className="h5">Galactic Digital Studios handles business card design very well. It is said that Portland, Oregon, is only the second-best place in the United States for coffee. With so many national chains and local shops, the opportunity to have your business card on a coffee shop wall is great. But it&rsquo;s not as great if you don&rsquo;t have the right design, a design that fits the overall theme and color scheme of your brand. That&rsquo;s why Galactic Digital Studios will go over details about your brand identity with you before designing your business card.</p>
+                    </div>
+                </div>
+                <div className="row">
+                    <div className="col-md-9 col-sm-12">
+                        <p className="h5">Something else that you may not realize is graphic design-centric is stickers. Let&rsquo;s face it; you see laptops with stickers on them all over Portland, Oregon. Why shouldn&rsquo;t your business also get in on that fun? Galactic Digital Studios will help design a sticker that will let people know who you are and what you do. And it&rsquo;s not just laptops; these stickers can be used for water bottles, music venue walls, even small furniture. No matter the vessel, the stickers will help get the word out about your business.</p>
+                        <p className="h5">And every graphic design service that Galactic Digital Studios provides comes with an option to shop around at various print shops in the Portland, Oregon area before shopping at one of the big box online stores. We believe in shopping local first for a number of reasons, not the least of which being timeliness and getting a quality product.</p>
+                        <h6 className="h5">Use the contact form below or the contact info found on this site to learn more about our graphic design services in the Portland, Oregon area and surrounding communities.</h6>
+                    </div>
+                    <div className="col-md-3 d-none d-md-block">
+                        <div className="container-fluid">
+                            <div className="row">
+                                <div className="col">
+                                    <img src="https://galacticdigitalstudios.com/img/portland-oregon-graphic-design.jpg" width="100%" alt="Portland Oregon graphic design" />
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div className="row">
+                    <div className="col">
+                        <RequestForm selectedService="graphic" />
+                    </div>
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default Page;
---
+++ pages/services/portland-oregon-web-development.js
@@ -0,0 +1,97 @@
+import Head from "next/head"
+import Script from "next/script"
+import InsidePageHeader from "../../components/InsidePageHeader"
+import RequestForm from "../../components/RequestForm"
+
+const Page = () => {
+    return (
+        <>
+            <Head>
+                <title>Portland Oregon Web Development - Galactic Digital Studios</title>
+                <meta name="description" content="Simple 3 page websites advertising a local business. Multi-page websites that go a little deeper with content. Dynamic websites that grow with your business. Galactic Digital Studios has done and can do them all." />
+                <meta name="keywords" content="website design, website development, web development, web design, Portland, Oregon" />
+
+                <meta property="og:type" content="website" />
+                <meta property="og:url" content="https://galacticdigitalstudios.com/services/portland-oregon-web-development" />
+                <meta property="og:title" content="Portland Oregon Web Development - Galactic Digital Studios" />
+                <meta property="og:description" content="Simple 3 page websites advertising a local business. Multi-page websites that go a little deeper with content. Dynamic websites that grow with your business. Galactic Digital Studios has done and can do them all." />
+                <meta property="og:image" content="https://galacticdigitalstudios.com/img/gds-logo-short-transparent.png" />
+
+                <meta property="twitter:card" content="summary_large_image" />
+                <meta property="twitter:url" content="https://galacticdigitalstudios.com/services/portland-oregon-web-development" />
+                <meta property="twitter:title" content="Portland Oregon Web Development - Galactic Digital Studios" />
+                <meta property="twitter:description" content="Simple 3 page websites advertising a local business. Multi-page websites that go a little deeper with content. Dynamic websites that grow with your business. Galactic Digital Studios has done and can do them all." />
+                <meta property="twitter:image" content="https://galacticdigitalstudios.com/img/gds-logo-short-transparent.png" />
+                <link rel="icon" href="favicon.ico" />
+            </Head>
+            <Script strategy="afterInteractive" src="https://www.googletagmanager.com/gtag/js?id=G-B8904LJMQQ"/>
+            <Script
+                id='google-analytics'
+                strategy="afterInteractive"
+                dangerouslySetInnerHTML={{
+                    __html: `
+                    window.dataLayer = window.dataLayer || [];
+                    function gtag(){dataLayer.push(arguments);}
+                    gtag('js', new Date());
+                    gtag('config', 'G-B8904LJMQQ', {
+                        page_path: window.location.pathname,
+                    });
+                    `,
+                }}
+            />
+            <div className="container-fluid pb-5 mb-5">
+                <div className="row">
+                    <InsidePageHeader title="Portland Oregon Web Development" />
+                </div>
+                <div className="row">
+                    <div className="col">
+                        <h2>Web Development and Design For The Pacific Northwest</h2>
+                    </div>
+                </div>
+                <div className="row">
+                    <div className="col-md-3 d-none d-md-block">
+                        <div className="container-fluid">
+                            <div className="row">
+                                <div className="col">
+                                    <img src="https://galacticdigitalstudios.com/img/web-development-l.jpg" width="100%" alt="Two monitors with web development code on them" />
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="col-md-9 col-sm-12">
+                        <p className="h5">Small business web development doesn&rsquo;t have to be complicated or complex. Galactic Digital Studios wants to help ease the process for you, the small business owner, to have a clean, functioning, searchable website that you are not only happy to show off to friends and family, but you are proud to show off to the world.</p>
+                        <p className="h5">And now, for the first time, Galactic Digital Studios brings their web development services to the Pacific Northwest and Portland, Oregon. There&rsquo;s so much to enjoy and explore in Portland, Oregon: from the colorful scene of Old Town to Washington Park and the Oregon Zoo to the tamer and less chaotic east side, Portland, Oregon has so much more to offer than one can expect.</p>
+                        <p className="h5">So in a town that offers so much and in such a variety of ways, why should your Portland, Oregon based business feel any less? Small businesses based in the Portland, Oregon area shouldn&rsquo;t have to settle for cookie cutter templated sites when the town motto is &ldquo;get weird&rdquo;. Galactic Digital Studios can offer more than just a cookie cutter template site; we have experienced web development professionals that can take your online business to that next level.</p>
+                        <p className="h5">Our web development team will go over every detail needed to help build the foundation for what will likely be your online billboard: your website. We know how important it is that you leave a lasting and positive impression on your customers, be it they&rsquo;re based in Portland, Oregon or anywhere in the United States.</p>
+                    </div>
+                </div>
+                <div className="row">
+                    <div className="col-md-9 col-sm-12">
+                        <p className="h5">We start the web development process by going over the details of what your business brand is: the name, colors, hours of service, really any information that could prove to be useful to build the website. We&rsquo;ll go over with you any available names out there that fit what your business name is. From there we determine with you what the best options are for your website: CMS vs static site, how many pages, will there be ecommerce involved, etc.</p>
+                        <p className="h5">Galactic Digital Studios is also driven to make sure that your website is responsive and mobile-friendly. What that means is that your site will come up clean no matter what device or screen you view it on. Technology has come to where websites can now adjust based on the screen size of the device you&rsquo;re viewing it on. Using that information, we can use our web development services build websites that will adjust from laptop to desktop to mobile.</p>
+                        <p className="h5">Don&rsquo;t think that our web development services stop at what you see on your phone or laptop. In addition to full front-end services, the web development team at Galactic Digital Studios also offers database and content management services. Our dedicated professionals will coach you on how to add new content and even edit the content that&rsquo;s already on there. This comes especially in handy when you are promoting a sale or limited promotion on your site. The idea behind coaching the client on making their own edits is so that if a change needs to be made quickly, the client is able to do so without needing to rely heavily on the web development professional to do it.</p>
+                        <p className="h5">As for the database side of things, our team of web development professionals will continue to monitor your database to make sure that everything is saved and backed up in a timely manner. And if there&rsquo;s ever an issue, our web development team is usually on top of things to troubleshoot and determine where the issue is located.</p>
+                        <p className="h5">Galactic Digital Studios even offers Google Analytics and search engine optimization in addition to front-end web development services. After the site is launched, our work is not over; many could argue that that&rsquo;s just the beginning. Our team of web development professionals will go over what keywords will likely be targeted in our SEO efforts, then go over Google Analytics to determine where you&rsquo;re struggling and help focus on your strong points.</p>
+                        <h6 className="h5">Your local small business in Portland, Oregon deserves to stand out among the crowd; why not hire a web development professional to create a website that helps you stand out? Use the contact form below or the contact info found on this website and let Galactic Digital Studios handle all of your web development needs.</h6>
+                    </div>
+                    <div className="col-md-3 d-none d-md-block">
+                        <div className="container-fluid">
+                            <div className="row">
+                                <div className="col">
+                                    <img src="https://galacticdigitalstudios.com/img/portland-oregon-web-development.jpg" width="100%" alt="Portland Oregon web development" />
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div className="row">
+                    <div className="col">
+                        <RequestForm selectedService="website" />
+                    </div>
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default Page;
